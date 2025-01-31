4f534d33710465e65949b924962a06c7be1c03bd
MarekLukk <mareklukk8@gmail.com>
2025-01-31 09:22:34 +0200

    Update opilane_Localhost_XAMMP.sql

--- opilane_Localhost_XAMMP.sql
+++ opilane_Localhost_XAMMP.sql
@@ -44,4 +44,18 @@ VALUES(
 'aadress',
 4.4)
 
+CREATE TABLE language
+(
+ID int NOT NULL PRIMARY KEY,
+Code char(3) NOT NULL,
+Language varchar(50) NOT NULL,
+IsOfficial bit,
+Percentage smallint
+);
+insert into keelevalik(valikuNimetus, opilaneId, language)
+values
+('valik B', 1, 2), ('valik D', 1, 4),
+('valik A', 2, 1), ('valik B', 2, 2),
+('valik C', 3, 3), ('valik D', 3, 4),
+('valik A', 4, 1), ('valik D', 4, 4)
 
