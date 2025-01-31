bb6e3d857a27978472404ebd52c9be4594a645ff
MarekLukk <mareklukk8@gmail.com>
2025-01-31 09:13:27 +0200

    Update opilane_Localhost_XAMMP.sql

--- opilane_Localhost_XAMMP.sql
+++ opilane_Localhost_XAMMP.sql
@@ -1,5 +1,5 @@
 CREATE TABLE opilane(
-opilaneId int primary key identity(1,1),
+opilaneId int primary key AUTO_INCREMENT,
 eesnimi varchar(25) not null,
 perenimi varchar(25) not null,
 synniaeg date, 
@@ -7,5 +7,41 @@ stip bit,
 aadress text,
 keskmine_hinne decimal(2,1)
 );
+
+INSERT INTO opilane(
+eesnimi,
+perenimi,
+synniaeg,
+stip,
+aadress,
+keskmine_hinne)
+VALUES(
+'Marek',
+'Lukk',
+'2004-10-27',
+1,
+'aadress',
+4.5),
+(
+'Pork',
+'Maksonik',
+'2004-10-27',
+1,
+'aadress',
+4.2),
+(
+'Pork',
+'Maksonik',
+'2004-10-27',
+1,
+'aadress',
+4.4),
+(
+'Mark',
+'Gabella',
+'2000-4-20',
+1,
+'aadress',
+4.4)
 
 
