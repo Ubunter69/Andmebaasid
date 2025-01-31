fe1433db4f2d926c297d9bd0b3493d46683e9768
MarekLukk <mareklukk8@gmail.com>
2025-01-31 11:41:43 +0200

    Update OpilaneSQL_Server.sql

--- OpilaneSQL_Server.sql
+++ OpilaneSQL_Server.sql
@@ -59,7 +59,71 @@ select * from opilane;
 UPDATE opilane SET aadress='Tartu'
 WHERE opilaneId=3
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
-Vigane kood
+
+  CREATE DATABASE epoodLukk
+use epoodLukk
+
+CREATE TABLE Category (
+idCategory int primary key identity(1,1),
+CategoryName varchar(25) UNIQUE
+);
+SELECT * FROM Category;
+INSERT INTO Category (CategoryName)
+values ('jook'), ('söök')
+
+--TABELI struktuuri muutmine-->uue veergu lisamine
+ALTER TABLE Category ADD test int;
+
+--TABELI struktuuri muutmine-->VEERGU KUSTUTAMINE
+ALTER TABLE Category drop column test;
+
+Create table Product ( 
+idProduct int primary key identity(1,1),
+ProductName varchar(20) UNIQUE,
+idCategory int ,
+Foreign key (idCategory) references Category(idcategory),
+Price DECIMAL(5,2)
+);
+SELECT * FROM Category;
+SELECT *FROM product;
+drop table Product;
+insert into Product (ProductName, idCategory, Price)
+VALUES ('SakuMõdu',1, 0.99)
+
+ALTER TABLE Category ADD Mööbel varchar(20);
+ALTER TABLE Category drop column Mööbel;
+alter TABLE Category ADD Mööbel varchar(20);
+--INSERT INTO Category (Mööbel)
+--VALUES ('Toolid'), ('Diivanid');
+
+CREATE TABLE Sale (
+idSale int primary key identity(1,1),
+idProduct INT,
+Foreign key (idProduct) references Product (idProduct),
+idCustomer INT,
+Count_ INT,
+DateOfSale date);
+
+
+ 
+--Kustutab kõik kirjed
+----Foreign key (idCustomer) references Customer(idCustomer),
+DELETE FROM table sale
+CREATE TABLE Customer (
+IdCustomer int primary key identity(1,1),
+Name varchar(25),
+contact text)
+--tabeli muutmine -->FK lisamine
+alter table sale add foreign key (idCustomer) references Customer(idCustomer)
+INSERT INTO Customer(Name,contact)
+VALUES('Martin', '+3743531634')
+
+INSERT INTO Sale (idProduct, idCustomer, Count_, DateOfSale)
+values ('5','3','3','2018-11-10'),
+(5,2,10,'2018-11-11')
+  
+  ----------------------------------------------------------------------------
+  Vigane kood
 Create table oppimine(
 oppimineId int primary key identity(1,1),
 aine varchar(12) not null,
