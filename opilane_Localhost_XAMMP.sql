7d24c6ee40feca782081c70928039d1a7e8259da
MarekLukk <mareklukk8@gmail.com>
2025-01-31 09:12:06 +0200

    Update opilane_Localhost_XAMMP.sql

--- opilane_Localhost_XAMMP.sql
+++ opilane_Localhost_XAMMP.sql
@@ -1,1 +1,11 @@
+CREATE TABLE opilane(
+opilaneId int primary key identity(1,1),
+eesnimi varchar(25) not null,
+perenimi varchar(25) not null,
+synniaeg date, 
+stip bit,
+aadress text,
+keskmine_hinne decimal(2,1)
+);
+
 
