d7e6c4b2f793de6813ffaf78bb5271832dbfa7c8
MarekLukk <mareklukk8@gmail.com>
2025-02-07 10:50:47 +0200

    Update Protseduur.sql

--- Protseduur.sql
+++ Protseduur.sql
@@ -175,6 +175,118 @@ END;
 EXEC stipVahetus 4, 0;
 drop procedure stipVahetus;
 ------------------------------------------------------------------------------------------------------------------------------------
+CREATE DATABASE protseduurLukk;
+use protseduurLukk;
+create table linn(
+linnId int PRIMARY KEY identity(1,1),
+linnNimi varchar(30),
+rahvaArv int);
+SELECT * from linn; 
+insert into linn(linnNimi, rahvaArv)
+values ('Tartu', 30000);
+
+--('Tallinn', 60000), ('Pärnu', 40000), ('Narva', 35000);
+CREATE PROCEDURE lisaLinn
+@linnNimi varchar(30),
+@rahvaArv int
+AS
+BEGIN
+
+insert into linn(linnNimi, rahvaArv)
+values (@linnNimi, @rahvaArv);
+select* from linn;
+
+END
+exec lisaLinn Pldiski, 36362
+
+--uue Veeru Lisamine
+ALTER TABLE linn ADD test int;
+--veeru kustutamine
+ALTER TABLE linn drop COLUMN test;
+
+create procedure veeruLisaKustuta
+@valik varchar(20),
+@veerunimi varchar(20),
+@tüüp varchar (20) =null
+
+AS
+BEGIN
+Declare @sqltegevus as varchar(max)
+set @sqltegevus=case
+when @valik='add' then concat('ALTER TABLE linn ADD ', @veerunimi, ' ', @tüüp)
+when @valik='drop' then concat('ALTER TABLE linn DROP COLUMN ', @veerunimi)
+END;
+print @sqltegevus;
+begin
+EXEC (@sqltegevus);
+END;
+END;
+
+--KUTSE
+EXEC veeruLisaKustuta @valik='add', @veerunimi='test3', @tüüp='int';
+select * from linn;
+
+EXEC veeruLisaKustuta @valik='drop', @veerunimi='test3';
+select * from linn;
+
+create procedure veeruLisaKustutaTabelis
+@valik varchar(20),
+@tabelinimi varchar(20),
+@veerunimi varchar(20),
+@tüüp varchar (20) =null
+
+AS
+BEGIN
+Declare @sqltegevus as varchar(max)
+set @sqltegevus=case
+when @valik='add' then concat('ALTER TABLE ', @tabelinimi, ' ADD ', @veerunimi, ' ', @tüüp)
+when @valik='drop' then concat('ALTER TABLE ', @tabelinimi, ' DROP COLUMN ', @veerunimi)
+END;
+print @sqltegevus;
+begin
+EXEC (@sqltegevus);
+END;
+END;
+--KUTSE
+EXEC veeruLisaKustutaTabelis @valik='add', @tabelinimi='linn', @veerunimi='test3', @tüüp='int';
+select * from linn;
+
+EXEC veeruLisaKustutaTabelis @valik='drop', @tabelinimi='linn', @veerunimi='test3';
+select * from linn;
+DROP PROCEDURE veeruLisaKustutaTabelis;
+
+--protseduur tingimusega
+CREATE PROCEDURE rahvaHinnang
+@piir int
+
+
+AS
+BEGIN
+SELECT linnNimi, rahvaArv, IIF(rahvaArv<@piir, 'Väike linn', 'Suur linn') as Hinnang
+FROM linn;
+
+
+END;
+drop procedure rahvaHinnang;
+exec rahvaHinnang 60000;
+--Agregaat fumktsioonid: sum(), AVG(), MIN(), MAX(), COUNT()
+
+CREATE PROCEDURE KokkuRahvaarv
+
+
+
+AS
+BEGIN
+SELECT SUM(rahvaArv) AS 'kokku rahvaarv', AVG(rahvaarv) as 'keskmine rahvaArv', count(*) AS 'linnade arv'
+from linn;
+END;
+--через AS можно давать новое название столбцу
+--count(*) звездочка считает количество заполненных строчек
+
+EXEC KokkuRahvaarv;
+drop procedure KokkuRahvaarv;
+
+------------------------------------------------------------------------------------------------------------------------------------
 Kasutame XAMPP / localhost
 create table linn(
 linnId int PRIMARY KEY AUTO_INCREMENT,
@@ -195,4 +307,4 @@ select* from linn;
 END
 
 
-
+Syntax
